5382470e17811df1cc311461b56ba4dc01d29cc6
Nareshkumar124 <nkk124421@gmail.com>
2024-03-30 19:52:06 +0530

    Change in theme

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { MdDarkMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
 
 const ToggleTheme = ({ classname }) => {
   const [themeMode, setThemeMode] = useState('dark');
@@ -26,21 +28,26 @@ const ToggleTheme = ({ classname }) => {
   }, [themeMode]);
 
   return (
-    <>
-      <label className={`inline-flex items-center cursor-pointer ${classname}`}>
-        <input
-          type='checkbox'
-          value=''
-          className='sr-only peer'
-          onChange={onChangeTheme}
-          checked={themeMode === 'dark'}
-        />
-        <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-        <span className='ms-3 text-lg font-medium text-gray-900 dark:text-gray-300'>
-          Dark Mode
-        </span>
-      </label>
-    </>
+      <>
+          <label
+              className={`inline-flex items-center cursor-pointer ${classname}`}>
+              <input
+                  type="checkbox"
+                  value=""
+                  className="sr-only peer"
+                  onChange={onChangeTheme}
+                  checked={themeMode === "dark"}
+              />
+              {themeMode == "dark" ? (
+                  <MdDarkMode size={"30px"} />
+              ) : (
+                  <MdLightMode size={"30px"} />
+              )}
+              <span className="ms-3 text-lg font-medium text-gray-900 dark:text-gray-300">
+                  Theme
+              </span>
+          </label>
+      </>
   );
 };
 
