0fe645f4e63a77afe6ddef9c6b7edd7bf6940169
Nareshkumar124 <nkk124421@gmail.com>
2024-03-30 11:14:51 +0530

    added video api

--- test.js
+++ test.js
@@ -1,33 +1,90 @@
-import axios from "axios";
+import axios from 'axios';
+import fs from 'fs';
 
 const token =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MDZiMmMzYWZlOTQ2NjJlODEyNGVjMSIsInVzZXJuYW1lIjoibmtrIiwiZW1haWwiOiJua0BnbWFpbC5jb20iLCJleHAiOjE3MTE3MTY5MDV9.MefsACAL4QJEMDMAOzextZRsd0aZizuZiEoQe6ObYEo";
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MDM5ODI2ZGY4M2Q0NDc0Y2Y5NWQyMSIsInVzZXJuYW1lIjoibmFyZXNoIiwiZW1haWwiOiJ1c2VyQGV4YW1wbGUuY29tIiwiZXhwIjoxNzExNzcwMDM4fQ.GnpeiieW9qeu1FbyEE_d7asxl7GppI6gjE2xbQSrjfY';
 
-class Auth {
-	constructor() {
-		this.instance = axios.create({
-			baseURL: "http://127.0.0.1:8000",
-			timeout: 1000,
-		});
-	}
+class Videos {
+    constructor() {
+        this.instance = axios.create({
+            baseURL: 'http://127.0.0.1:8000',
+            timeout: 1000,
+        });
+    }
 
-	createHeader() {
-		return { Authorization: `Bearer ${token}` };
-	}
+    createHeader() {
+        return { Authorization: `Bearer ${token}` };
+    }
 
-	async uploadInDb({ title, description, contenType }) {}
+    async uploadInDb({ title, description, contenType }) {
+        try {
+            
+            const req = await this.instance({
+                method: 'post',
+                url: '/api/videos/upload',
+                data: { title, description, contenType },
+                headers: this.createHeader(),
+            });
 
-	async uploadInAws(file, url) {}
+            return req.data;
+        } catch (error) {
+            throw error;
+        }
+    }
 
-	async afterUpload(status, fileUri) {}
+    async afterUpload(status, fileUri) {
+        try {
+            const req = await this.instance({
+                method: 'post',
+                url: '/api/videos/after-upload',
+                headers: this.createHeader(),
+                data: { status, fileUri },
+            });
 
-	async upload({ title, description, contenType, file}) {}
+            return req.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async uploadInAws({ file, url, onProgress }) {
+        const myHeaders = new Headers();
+        myHeaders.delete('Transfer-Encoding');
+        try {
+            const req = await axios.put(url, file, {
+                onUploadProgress: onProgress,
+                headers: myHeaders,
+            });
+            return req.status;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // async upload({ title, description, contenType, file }) {
+
+    // }
 }
 
-const auth = new Auth();
+const video = new Videos();
 
-auth.getUser().then((res) => {
-	console.log(res);
-});
+// video.uploadInDb({
+// 	"title":"v11",
+// 	"description":"v10",
+// 	"contenType":"video/mp4"
+// }).then((res)=>{
+// 	console.log(res)
+// })
+
+video
+    .uploadInAws({
+        file: fs.createReadStream('./v1.mp4'),
+        url: 'https://videotextalgo.s3.amazonaws.com/videos/ed21a484-4936-42ff-9b4b-daea7bc12e29/e77cf607-b731-4967-8d13-2a3eb3ff862e.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAYRUTEZQODWATY5FQ%2F20240330%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20240330T034836Z&X-Amz-Expires=5000&X-Amz-SignedHeaders=host&X-Amz-Signature=5f1a3d4021473a0b7ac4bc9beb614ab14ea23ce42b57d4370567988c9a05ff11',
+        onProgress: (p) => console.log(p),
+    })
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((e) => console.log(e));
 
 // export default auth;
