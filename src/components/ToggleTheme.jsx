28f06af3ace13a5e48ded640a66f06ee1d3592ed
gurparvez <gurparvez04@gmail.com>
2024-03-31 09:40:13 +0530

    toggle component

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -1,54 +1,54 @@
 import React, { useEffect, useState } from 'react';
-import { MdDarkMode } from "react-icons/md";
-import { MdLightMode } from "react-icons/md";
+import { MdDarkMode } from 'react-icons/md';
+import { MdLightMode } from 'react-icons/md';
 
 const ToggleTheme = ({ classname }) => {
-  const [themeMode, setThemeMode] = useState('dark');
+    const [themeMode, setThemeMode] = useState('dark');
 
-  const lightMode = () => {
-    setThemeMode('dark');
-  };
+    const lightMode = () => {
+        setThemeMode('dark');
+    };
 
-  const darkMode = () => {
-    setThemeMode('light');
-  };
+    const darkMode = () => {
+        setThemeMode('light');
+    };
 
-  const onChangeTheme = (e) => {
-    const darkModeStatus = e.currentTarget.checked;
-    if (darkModeStatus) {
-      lightMode();
-    } else {
-      darkMode();
-    }
-  };
+    const onChangeTheme = (e) => {
+        const darkModeStatus = e.currentTarget.checked;
+        if (darkModeStatus) {
+            lightMode();
+        } else {
+            darkMode();
+        }
+    };
 
-  useEffect(() => {
-    document.querySelector('html').classList.remove('light', 'dark');
-    document.querySelector('html').classList.add(themeMode);
-  }, [themeMode]);
+    useEffect(() => {
+        document.querySelector('html').classList.remove('light', 'dark');
+        document.querySelector('html').classList.add(themeMode);
+    }, [themeMode]);
 
-  return (
-      <>
-          <label
-              className={`inline-flex items-center cursor-pointer ${classname}`}>
-              <input
-                  type="checkbox"
-                  value=""
-                  className="sr-only peer"
-                  onChange={onChangeTheme}
-                  checked={themeMode === "dark"}
-              />
-              {themeMode == "dark" ? (
-                  <MdDarkMode size={"30px"} />
-              ) : (
-                  <MdLightMode size={"30px"} />
-              )}
-              <span className="ms-3 text-lg font-medium text-gray-900 dark:text-gray-300">
-                  Theme
-              </span>
-          </label>
-      </>
-  );
+    return (
+        <>
+            <label
+                className={`inline-flex items-center cursor-pointer ${classname}`}>
+                <input
+                    type='checkbox'
+                    value=''
+                    className='sr-only peer'
+                    onChange={onChangeTheme}
+                    checked={themeMode === 'dark'}
+                />
+                {themeMode == 'dark' ? (
+                    <MdDarkMode size={'30px'} />
+                ) : (
+                    <MdLightMode size={'30px'} />
+                )}
+                <span className='ms-3 text-lg font-medium text-gray-900 dark:text-gray-300'>
+                    Theme
+                </span>
+            </label>
+        </>
+    );
 };
 
 export default ToggleTheme;
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import ToggleTheme from './ToggleTheme';
 import Header from './Header.jsx';
 import Button from './Buttons/Button.jsx';
 import ButtonRed from './Buttons/ButtonRed.jsx';
+import Toggle from './Toggle.jsx';
 
-export { ToggleTheme, Header, Button, ButtonRed };
+export { ToggleTheme, Header, Button, ButtonRed, Toggle };
