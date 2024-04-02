7a29b6f947ac983e211418b49590c1cda6ee99e6
gurparvez <gurparvez04@gmail.com>
2024-04-03 00:32:21 +0530

    pretify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,16 @@ import { ButtonRed, Logo, ToggleTheme } from './index.js';
 import { NavLink } from 'react-router-dom';
 
 const Header = ({ classname }) => {
-  return (
-    <div className={`flex flex-row justify-between items-center ${classname}`}>
-      <NavLink to='/'>
-        <Logo classname='h-10 cursor-pointer' />
-      </NavLink>
-      <div className='flex *:px-5'>
-        <ToggleTheme />
-        <ButtonRed data='Logout' classname='text-lg' />
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className={`flex flex-row justify-between items-center ${classname}`}>
+            <NavLink to='/'>
+                <Logo classname='h-10 cursor-pointer' />
+            </NavLink>
+            <div className='flex'>
+                <ToggleTheme />
+            </div>
+        </div>
+    );
 };
 export default Header;
--- src/layouts/Login.jsx
+++ src/layouts/Login.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import { Button, Input, Logo, ShowError, Spinner } from '../components';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form';
 import auth from '../api/auth';
 import { login } from '../store/authSlice';
 
 const Login = () => {
-
-    const {register, handleSubmit} = useForm();
+    const { register, handleSubmit } = useForm();
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(false);
     const [msg, setMsg] = useState('');
@@ -30,29 +29,29 @@ const Login = () => {
         } catch (error) {
             setError(true);
             setMsg(error);
-            console.log("Error :: register user ::", error);
+            console.log('Error :: register user ::', error);
         }
-    }
+    };
 
     return (
-        <section className={`relative bg-gray-50 dark:bg-gray-900 ${isLoading ? 'pointer-events-none':'pointer-events-auto'}`}>
-            {
-                isLoading &&
-                <Spinner  />
-            }
-            <div className='flex flex-col items-center justify-center py-8 mx-auto md:h-screen lg:py-0'>
+        <section
+            className={`relative bg-gray-50 dark:bg-gray-900 ${isLoading ? 'pointer-events-none' : 'pointer-events-auto'}`}>
+            {isLoading && <Spinner />}
+            <div className='flex flex-col items-center justify-center py-8 mx-auto h-screen lg:py-0'>
                 <Logo classname='mb-6 text-xl font-semibold' />
                 <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
                     <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
                         <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
                             Create and account
                         </h1>
-                        <form onSubmit={handleSubmit(submit)} className='space-y-4 md:space-y-6'>
+                        <form
+                            onSubmit={handleSubmit(submit)}
+                            className='space-y-4 md:space-y-6'>
                             <Input
                                 label='Username'
                                 error={error}
                                 readonly={isLoading}
-                                {...register("username", {
+                                {...register('username', {
                                     required: true,
                                 })}
                             />
@@ -61,19 +60,18 @@ const Login = () => {
                                 type='password'
                                 error={error}
                                 readonly={isLoading}
-                                {...register("password", {
+                                {...register('password', {
                                     required: true,
                                 })}
                             />
-                            {
-                                error &&
-                                <ShowError error={msg} />
-                            }
+                            {error && <ShowError error={msg} />}
                             <Button
                                 data='Create Account'
                                 type='submit'
                                 bg={isLoading ? 'bg-blue-900' : 'bg-blue-700'}
-                                bgDark={isLoading ? 'bg-blue-800' : 'bg-blue-600'}
+                                bgDark={
+                                    isLoading ? 'bg-blue-800' : 'bg-blue-600'
+                                }
                                 classname='w-full'
                             />
                             <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
