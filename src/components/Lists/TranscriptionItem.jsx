1b15fd5a308b44d939daf3d5b4d4441226bd84cd
gurparvez <gurparvez04@gmail.com>
2024-04-25 19:50:29 +0530

    video item style

--- src/components/Lists/TranscriptionItem.jsx
+++ src/components/Lists/TranscriptionItem.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const TranscriptionItem = ({
     transcription,
-    isSelected,
+    isSelected = false,
     onSelect,
     classname,
 }) => {
     return (
         <div
-            className={` ${isSelected ? '' : ''} ${classname}`}
+            className={` ${isSelected ? '' : ''} hover:bg-gray-300 dark:hover:bg-gray-800 w-full px-4 py-2 border-b border-gray-200 transition-all first:rounded-t-lg last:rounded-b-lg last:border-none dark:border-gray-400 ${classname}`}
             onClick={() => {
                 onSelect(transcription?.time[0]);
             }}>
